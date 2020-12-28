0d55055c7b295f03b7a7ba061bc6361abff70d4d
Darlan Paz <darlan.mpaz@gmail.com>
2020-12-28 15:44:37 -0300

    View AddSpent

--- frontend/src/components/modalMenu.js
+++ frontend/src/components/modalMenu.js
@@ -4,14 +4,16 @@ import {FiPlusCircle, FiHome, FiGrid} from 'react-icons/fi'
 
 function ModalMenu() {
   return (
-    <div className="modal-acoes">
+    <div className="menu">
+          <div className="modal-acoes">
 
-      <Link to="/" className="modal-acao--home link"><FiHome className="icon"/></Link>
+          <Link to="/" className="modal-acao--home link"><FiHome className="icon"/></Link>
 
-      <Link to="/Spents" className="modal-acao--gastos link"><FiGrid className="icon"/></Link>
+          <Link to="/Spents" className="modal-acao--gastos link"><FiGrid className="icon"/></Link>
 
-      <Link className="modal-acao--nova link"><FiPlusCircle className="icon"/></Link>
-      
+          <Link to="/AddSpent" className="modal-acao--nova link"><FiPlusCircle className="icon"/></Link>
+
+</div>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 
 import App from './App'
 import Spents from './components/Spents'
+import AddSpent from './components/AddSpent'
 
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
@@ -11,6 +12,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Route path="/" exact={true} component={App} />
     <Route path="/Spents" component={Spents} />
+    <Route path="/AddSpent" component={AddSpent} />
   </BrowserRouter>,
   document.getElementById('root')
 );
